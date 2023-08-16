81c0a6166cab4a78a2c49cdf5704bea56a871322
AnshuMn-Singh <60657185+AnshuMn-Singh@users.noreply.github.com>
2023-08-17 01:20:41 +0530

    module 4 Getting Started with Spring Boot

---
+++ src/main/java/module4SpringBoot/a1Course/Course.java
@@ -0,0 +1,36 @@
+package module4SpringBoot.a1Course;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import java.io.Serializable;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class Course implements Serializable {
+    private static final long serialVerUUID = 1L;
+    private int id;
+    private String topic;
+    private String author;
+
+    public Course ( int id, String topic, String author ) {
+        this.id = id;
+        this.topic = topic;
+        this.author = author;
+    }
+
+    public void setId ( int id ) {
+        this.id = id;
+    }
+
+    public void setTopic ( String topic ) {
+        this.topic = topic;
+    }
+
+    public void setAuthor ( String author ) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString ( ) {
+        return "id=" + id + ", topic='" + topic + ", author='" + author;
+    }
+}
